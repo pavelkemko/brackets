981d78b0aa0c703294dc70157276aa304c83bd8a
pavelkemko <pavelkemko@gmail.com>
2023-02-09 18:34:12 +0200

    feat: finish task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let pareBrackets = [];
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    pareBrackets.push(bracketsConfig[i].join(''));
+  }
+  for (let j = 0; j < pareBrackets.length;) {
+    if (str.includes(pareBrackets[j])) {
+      str = str.replace(pareBrackets[j], '');
+      j = 0;
+    } else {j++}
+  }
+  console.log(str)
+  return str.length === 0;
 }
